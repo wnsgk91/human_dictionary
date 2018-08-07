a9eb2422dc0ef4e91a2b3595086f0293fa6cdf61
junha <junha_lee@ijunhaui-MacBook-Pro.local>
2018-08-07 19:10:02 +0900

    add vendor

--- main.js
+++ main.js
@@ -2,51 +2,74 @@
 var express = require('express');
 var app = express();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//jade 사용
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-
-
+//정리 이쁘게
+app.locals.pretty = true;
 
   app.listen(3000, function() {
-  	console.log('Connected');
+    console.log('Connected');
   });
+
+
+  app.get('/', function(req,res){
+
+    res.render('index.html');
+
+  })
+
+  app.get('/home', function(req,res){
+
+    res.render('home/home.html');
+    
+  })
+
+  app.get('/search',function(req, res){
+
+    res.render('search/search.html');
+
+  });
+
+  app.get('/favorite', function(req,res){
+
+    res.render('favorite/favorite.html');
+
+  });
+
+  app.get('/document', function(req,res){
+
+    res.render('document/document.html');
+
+  });
+
+
+  // 변수
+  var history_list = [];
+
+  // 검색버튼 눌렀을 때 호출되는 함수
+  function search_history(){
+
+    alert(history_list);
+    var a = history_list.length+1;
+
+    localStorage.setItem('search'+a,document.getElementById('search').value);
+    
+    history_list.push(localStorage.getItem('search'+a));
+
+    alert(history_list);
+
+  }
+
+    var arr = new Array();
+    var i =0;
+
+      function searching(){
+
+        alert(arr);
+
+        arr.push(i);
+        i++;
+
+    }
---
+++ vendor/jquery/show_fav_jqry.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#fav_btn").on("click", function () {
+        console.log("Restoring array data from LocalStorage");
+
+        myFa
+    })
+})
