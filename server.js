dfa14010d88a2cb21c6d31f995fd985bb2c9831e
sub <sostkr94@gmail.com>
2018-08-10 10:50:48 +0900

    import JSON file into MySQL using node.js

---
+++ server.js
@@ -0,0 +1,63 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var mysql = require('mysql');
+var fs = require('fs');
+
+
+// Configure MySQL connection
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '111111',
+	database: 'testdb'
+  });
+
+//Establish MySQL connection
+ 
+  connection.connect(function(err) {
+    if (err)
+      throw err
+   else {
+       console.log('Connected to MySQL');
+       // Start the app when connection is ready
+       app.listen(3000);
+       console.log('Server listening on port 3000');
+ }
+});
+
+
+function isEmptyEl(array, i) {
+   return !(array[i]);
+}
+
+app.get('/', function(req, res) {
+
+values = [];
+
+// String 형식으로 받음. 
+var jsondata = fs.readFileSync("C:/Users/sub/Documents/translation/test2.json", 'utf8');
+var modified = JSON.parse(jsondata.trim());
+
+
+for (var i = 0; i <= Object.keys(modified.diseases).length-1; i++) {
+      if (modified.diseases[i].Supervision === undefined ) {
+        modified.diseases[i].Supervision = "해당내용 없습니다.";
+        console.log(modified.diseases[i].Supervision);
+        values.push([modified.diseases[i].name, modified.diseases[i].GeneralDiscussion,modified.diseases[i].Symptoms,modified.diseases[i].Causes,modified.diseases[i].Diagnosis,modified.diseases[i].Treatment,modified.diseases[i].BibliographySite,modified.diseases[i].Etc,modified.diseases[i].Hospital, modified.diseases[i].Supervision]);    
+      }else{
+      console.log(modified.diseases[i].Supervision);
+      values.push([modified.diseases[i].name, modified.diseases[i].GeneralDiscussion,modified.diseases[i].Symptoms,modified.diseases[i].Causes,modified.diseases[i].Diagnosis,modified.diseases[i].Treatment,modified.diseases[i].BibliographySite,modified.diseases[i].Etc,modified.diseases[i].Hospital, modified.diseases[i].Supervision]);
+        }
+      }
+
+// MySQL insert query  
+connection.query('INSERT INTO test3 (name, general_discussion, symptoms, casues, diagnosis, treatment, bibliography_site, etc, hospital, supervision) VALUES ?', [values], function(err,result) {
+  if(err) {
+                    console.log(err.toString());
+          } else {
+              console.log("Success");
+     }
+    });
+});
