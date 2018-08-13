1bc3a83f6635d9d6a4e5eaea9c95335317e37dbb
junha <junha_lee@ijunhaui-MacBook-Pro.local>
2018-08-13 16:03:20 +0900

    즐겨찾기 화면에서 즐겨찾기 삭제하기

--- public/javascripts/favorite_manager.js
+++ public/javascripts/favorite_manager.js
@@ -35,35 +35,6 @@ $(function () {
   });
 });
 
-// document 페이지에서 즐겨찾기 삭제 버튼 눌렀을 때
-$(function () {
-  $("#rmv").on("click", function () {
-    const favToRmv = $("#title")[0].textContent;
-    console.log(favToRmv);
-    myFavoriteList = JSON.parse(localStorage.getItem("myFavList"));
-
-    if (myFavoriteList !== null) {
-      for (let i = 0; i < myFavoriteList.length; i++) {
-        if (favToRmv === myFavoriteList[i]) {
-          var index = i;
-          var exists = true;
-        }
-      }
-      if(exists){
-        delete myFavoriteList[index];
-        localStorage.setItem("myFavList", JSON.stringify(myFavoriteList));
-        alert("즐겨찾기 목록에서 삭제되었습니다");
-
-      } else {
-        alert("이미 제거되었습니다.");
-      }
-
-    } else {
-      alert("즐겨찾기 목록이 없습니다");
-    }
-  });
-});
-
 // favorite 페이지에 목록 로딩해주기
 function loadFavorites() {
   //console.log("즐겨찾기 목록 불러오는 중");
@@ -75,11 +46,36 @@ function loadFavorites() {
     let output = '';
     for (let i = 0; i < loadList.length; i++){
       if(loadList[i] !== null){
-        output += '<a href = "/document/'+ loadList[i] + '" class="list-group-item list-group-item-action">'+ loadList[i] + '</a>';
+        output += '<li class="list-group-item"><a href = "/document/' + loadList[i] + '">'+ loadList[i] + '</a><button type="submit" class="btn btn-outline-info float-right" onclick="rmv_favorite();">즐겨찾기 삭제</button></li>';
       }
     }
     $("#favorite_list")[0].innerHTML = output;//jqeury selector 찾아보기, appendChild
   }
+}
+
+function rmv_favorite(){
+    loadList = JSON.parse(localStorage.getItem("myFavList"));
+    myFavoriteList = JSON.parse(localStorage.getItem("myFavList"));
+
+    if (myFavoriteList !== null) {
+      for (let i = 0; i < myFavoriteList.length; i++) {
+        if (loadList[i] === myFavoriteList[i]) {
+          var index = i;
+          var exists = true;
+        }
+      }
+      if(exists){
+        delete myFavoriteList[index];
+        localStorage.setItem("myFavList", JSON.stringify(myFavoriteList));
+        alert("즐겨찾기 목록에서 삭제되었습니다");
+        window.location.reload(true);
+
+      } else {
+        alert("이미 제거되었습니다.");
+      }
+    } else {
+      alert("즐겨찾기 목록에 없습니다");
+    }
 }
 
 //favorite 페이지 목록 모두 지우기.
