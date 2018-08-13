2552e5dab01f94824bda4647b9ac616d6da9da0e
thdud <soyeong1744@gmail.com>
2018-08-13 14:30:04 +0900

    코드 정리

--- public/javascripts/search_manager.js
+++ public/javascripts/search_manager.js
@@ -1,69 +1,39 @@
 //search page
 // form id : search_form
 // input id : search_search
+// 사전목록 id : dictionary
 
 //home page
 // form id : search_form_home
 // input id : home_search
 // 검색하기 버튼 id : history_log
 
-//search page
-// 사전목록 id : dictionary
-
-/*if(document.getElementById('home_search') !== null){
-    var pass_input = document.getElementById('home_search').value;
-}*/
-
-
 // home 페이지에서 검색할 때.
 function search_wizard_home() {
-    var input; //, filter, dict_list, li_tag, a_tag, i;
-    input = JSON.parse(localStorage.getItem('search_history')); // input tag id
-    //filter = input[-1].value.toUpperCase(); // 대문자로 바뀐 input 값(검색어) 가져오기
-    document.getElementById('search_search').value = input[-1];
-
-    search_btn_search.value = input[-1];
-    search_btn_search.addEventListener('click', search_wizard);
-
-
-/*    dict_list = document.getElementById('dictionary'); // 사전목록 가져오기
-    li_tag = dict_list.getElementsByTagName('li'); // 목록 중 li tag 가져오기
-    for (i = 0; i < li_tag.length; i++) {
-        a_tag = li_tag[i].getElementsByTagName("a")[0]; // li tag 안의 a 태그
-        if (a_tag.innerHTML.toUpperCase().indexOf(filter) > -1) { //a 태그 안의 내용 비교.
-            // 각 목록들이 검색어를 포함하는가? 포함 하면 인덱스 위치 반환, 포함 안하면 -1 반환
-            li_tag[i].style.display = "";// 보여주기
-        } else {
-            li_tag[i].style.display = "none";// 숨기기
-        }
-    }*/
+  let input = JSON.parse(localStorage.getItem('search_history')); // 검색 히스토리 가져오기
+  //filter = input[-1].value.toUpperCase(); // 대문자로 바뀐 input 값(검색어) 가져오기
+  document.getElementById('search_search').value = input[-1]; // 검색 히스토리의 제일 마지막 값을 전달하기.
+  search_btn_search.value = input[-1];
+  search_btn_search.addEventListener('click', search_wizard);
 }
 
-
-    /*var keyword = $('form').serialize();
-    console.log(keyword);*/
-
-   /* var keyword = document.forms['search_form_home'].elements['search_keyword'].value;
-    console.log(keyword);*/
-
-
-// search 페이지에서 검색할 때
+// search 페이지에서 필터링 기능
 function search_wizard() {
-    var input, filter, dict_list, li_tag, a_tag, i;
-    input = document.getElementById('home_search'); // input tag id
-    filter = input.value.toUpperCase(); // 대문자로 바뀐 input 값(검색어) 가져오기
+  let input, filter, dict_list, li_tag, a_tag, i;
+  input = document.getElementById('home_search'); // input tag id
+  filter = input.value.toUpperCase(); // 대문자로 바뀐 input 값(검색어) 가져오기
 
-    dict_list = document.getElementById('dictionary'); // 사전목록 가져오기
-    li_tag = dict_list.getElementsByTagName('li'); // 목록 중 li tag 가져오기
-    for (i = 0; i < li_tag.length; i++) {
-        a_tag = li_tag[i].getElementsByTagName("a")[0]; // li tag 안의 a 태그
-        if (a_tag.innerHTML.toUpperCase().indexOf(filter) > -1) { //a 태그 안의 내용 비교.
-            // 각 목록들이 검색어를 포함하는가? 포함 하면 인덱스 위치 반환, 포함 안하면 -1 반환
-            li_tag[i].style.display = "";// 보여주기
-        } else {
-            li_tag[i].style.display = "none";// 숨기기
-        }
+  dict_list = document.getElementById('dictionary'); // 사전목록 가져오기
+  li_tag = dict_list.getElementsByTagName('li'); // 목록 중 li tag 가져오기
+  for (i = 0; i < li_tag.length; i++) {
+    a_tag = li_tag[i].getElementsByTagName("a")[0]; // li tag 안의 a 태그
+    if (a_tag.innerHTML.toUpperCase().indexOf(filter) > -1) { //a 태그 안의 내용 비교.
+      // 각 목록들이 검색어를 포함하는가? 포함 하면 인덱스 위치 반환, 포함 안하면 -1 반환
+      li_tag[i].style.display = "";// 보여주기
+    } else {
+      li_tag[i].style.display = "none";// 숨기기
     }
+  }
 }
 
 const search_btn_search = document.forms['search_form'].querySelector('input');
@@ -71,8 +41,6 @@ search_btn_search.addEventListener('keyup', search_wizard);
 
 const search_btn_home = document.querySelector('#history_log');
 search_btn_home.addEventListener('click', search_wizard_home);
-//search_btn_home.addEventListener('click', location.href = '/search');
-
 
 /*
 const dictionary = document.querySelector('#dictionary');
--- server.js
+++ server.js
@@ -1,63 +1,58 @@
-
 var express = require('express');
 var app = express();
 var path = require('path');
 var mysql = require('mysql');
 var fs = require('fs');
 
-
 // Configure MySQL connection
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'dlwnsgk94',
+	//password: 'dlwnsgk94',
+  password: '1648',
 	database: 'dic'
-  });
-
-//Establish MySQL connection
- 
-  connection.connect(function(err) {
-    if (err)
-      throw err
-   else {
-       console.log('Connected to MySQL');
-       // Start the app when connection is ready
-       app.listen(3000);
-       console.log('Server listening on port 3000');
- }
 });
 
+//Establish MySQL connection
+connection.connect(function(err) {
+  if (err)
+    throw err;
+  else {
+     console.log('Connected to MySQL');
+     // Start the app when connection is ready
+     app.listen(3000);
+     console.log('Server listening on port 3000');
+  }
+});
 
 function isEmptyEl(array, i) {
    return !(array[i]);
 }
 
 app.get('/', function(req, res) {
+  values = [];
 
-values = [];
+  // String 형식으로 받음.
+  var jsondata = fs.readFileSync("./test.json", 'utf8');
+  var modified = JSON.parse(jsondata.trim());
 
-// String 형식으로 받음. 
-var jsondata = fs.readFileSync("./test.json", 'utf8');
-var modified = JSON.parse(jsondata.trim());
-
-
-for (var i = 0; i <= Object.keys(modified.diseases).length-1; i++) {
-      if (modified.diseases[i].Supervision === undefined ) {
-        modified.diseases[i].Supervision = "해당내용 없습니다.";
-        console.log(modified.diseases[i].Supervision);
-        values.push([modified.diseases[i].name, modified.diseases[i].GeneralDiscussion,modified.diseases[i].Symptoms,modified.diseases[i].Causes,modified.diseases[i].Diagnosis,modified.diseases[i].Treatment,modified.diseases[i].BibliographySite,modified.diseases[i].Etc,modified.diseases[i].Hospital, modified.diseases[i].Supervision]);    
-      }else{
+  for (var i = 0; i <= Object.keys(modified.diseases).length-1; i++) {
+    if (modified.diseases[i].Supervision === undefined ) {
+      modified.diseases[i].Supervision = "해당내용 없습니다.";
       console.log(modified.diseases[i].Supervision);
       values.push([modified.diseases[i].name, modified.diseases[i].GeneralDiscussion,modified.diseases[i].Symptoms,modified.diseases[i].Causes,modified.diseases[i].Diagnosis,modified.diseases[i].Treatment,modified.diseases[i].BibliographySite,modified.diseases[i].Etc,modified.diseases[i].Hospital, modified.diseases[i].Supervision]);
-        }
-      }
+    } else {
+    console.log(modified.diseases[i].Supervision);
+    values.push([modified.diseases[i].name, modified.diseases[i].GeneralDiscussion,modified.diseases[i].Symptoms,modified.diseases[i].Causes,modified.diseases[i].Diagnosis,modified.diseases[i].Treatment,modified.diseases[i].BibliographySite,modified.diseases[i].Etc,modified.diseases[i].Hospital, modified.diseases[i].Supervision]);
+    }
+  }
 
-// MySQL insert query  
-connection.query('INSERT INTO diseases (name, sum, symptom, cause, diagnosis, treat, site, etc, institution, supervisor) VALUES ?', [values], function(err,result) {
-  if(err) {
-                    console.log(err.toString());
-          } else {
-              console.log("Success");
-     }
+  // MySQL insert query
+  connection.query('INSERT INTO diseases (name, sum, symptom, cause, diagnosis, treat, site, etc, institution, supervisor) VALUES ?', [values], function(err,result) {
+    if(err) {
+      console.log(err.toString());
+    } else {
+      console.log("Success");
+    }
     });
 });
