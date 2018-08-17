dd9c6469f2546b9fdc65d0b558a5f1b8d90961ed
junha <junha_lee@ijunhaui-MacBook-Pro.local>
2018-08-17 21:29:51 +0900

    document에서 즐겨찾기 추가 / home.css

--- public/javascripts/favorite_manager.js
+++ public/javascripts/favorite_manager.js
@@ -54,15 +54,15 @@ function loadFavorites() {
     let output = '';
     for (let i = 0; i < loadList.length; i++){
       if(loadList[i] !== null){
-        output += '<li class="list-group-item"><a id = "title" href = "/document/' + loadList[i] + '">'+ loadList[i] + '</a>' +
-          '<svg id="i-star" class="float-right" viewBox="0 0 32 32" width="20" height="20" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">' +
-          '<path d="M16 2 L20 12 30 12 22 19 25 30 16 23 7 30 10 19 2 12 12 12 Z" />' +
-          '</svg></li>';
+        output += '<li class="list-group-item"><a id = "title" href = "/document/' + loadList[i] + '">'+ loadList[i] + '</a><div class="star_solid"></div></li>';
       }
     }
     showFav.innerHTML = output;
   }
 }
+
+
+
 
 // 즐겨찾기 삭제.
 function rmv_favorite(){
@@ -86,6 +86,9 @@ function rmv_favorite(){
     alert("즐겨찾기 목록에 없습니다");
   }
 }
+
+
+
 
 function get_item(){
   return JSON.parse(localStorage.getItem("myFavList"));
